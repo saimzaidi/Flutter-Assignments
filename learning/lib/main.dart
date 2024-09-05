2667c2c790bc472cde6541d73eb112e45dd2343f
saimzaidi <saimzaidi719@gmail.com>
2024-09-05 05:09:56 -0700

    Uploading assignment no 2

---
+++ learning/lib/main.dart
@@ -0,0 +1,301 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        // appBar: AppBar(),
+        body: Center(
+          child: Container(
+          color: Colors.brown,
+          width: 800,
+          height: 600,
+          child: Column(
+          children: [
+
+            Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+              ],
+            ),
+
+            Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+              ],
+            ),
+
+            Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+              ],
+            ),
+
+            Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+              ],
+            ),
+
+            Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+              ],
+            ),
+
+            Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: const Color.fromARGB(255, 192, 188, 188)
+                ),
+              ],
+            ),
+
+          ],
+        ),
+        ),
+        )
+      ),
+    );
+  }
+}
